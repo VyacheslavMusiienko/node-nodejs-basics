4e10c9ea17a9c40eba5c552fee198817ae252c41
VyacheslavMusiienko <chuvachok98@gmail.com>
2023-06-15 11:32:45 +0300

    feat: implements functions in stream folder

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,5 +1,19 @@
+import { createReadStream } from 'fs';
+import { join, dirname } from 'path';
+import { fileURLToPath } from 'url';
+import { stdout } from 'process';
+
 const read = async () => {
-    // Write your code here 
+  const filePath = join(dirname(fileURLToPath(import.meta.url)),'files','fileToRead.txt');
+  const readStream = createReadStream(filePath);
+
+  readStream.on('data', (chunk) => {
+    stdout.write(chunk);
+  });
+
+  readStream.on('error', (err) => {
+    console.error(`Error reading file: ${err}`);
+  });
 };
 
 await read();
--- src/streams/write.js
+++ src/streams/write.js
@@ -1,5 +1,17 @@
+import { createWriteStream } from 'fs';
+import { dirname, join } from 'path';
+import { exit, stdin, stdout } from 'process';
+import { fileURLToPath } from 'url';
+
 const write = async () => {
-    // Write your code here 
+  const filePath = join(dirname(fileURLToPath(import.meta.url)),'files','fileToWrite.txt');
+  const writeStream = createWriteStream(filePath);
+
+  process.on('exit', () => {stdout.write('You\'re done!\n')});
+  process.on('SIGINT', () => exit());
+
+  stdin.pipe(writeStream);
+
 };
 
 await write();
