aa7409f6ad7e6e0dd9787fb9f8aebf859de8f355
VyacheslavMusiienko <chuvachok98@gmail.com>
2023-06-16 15:16:03 +0300

    feat: implement function in worker folder

--- src/wt/main.js
+++ src/wt/main.js
@@ -1,5 +1,20 @@
+import { cpus } from 'os';
+import { dirname, join } from 'path';
+import { fileURLToPath } from 'url';
+import { Worker } from 'worker_threads';
+
+const workerPath = join(dirname(fileURLToPath(import.meta.url)), 'worker.js');
 const performCalculations = async () => {
-    // Write your code here
+  const calcNthFibonacci = (workerData) => new Promise((res) => {
+    const worker = new Worker(workerPath, {workerData});
+    worker.on('message', (data) => res({status: 'resolved', data}));
+    worker.on('error', () => res({status: 'error', data: null}));
+  });
+
+  const calculation = new Array(cpus().length).fill(null).map((_, index) => calcNthFibonacci(index + 10));
+  const data = await Promise.all(calculation);
+
+  console.log(data);
 };
 
 await performCalculations();
