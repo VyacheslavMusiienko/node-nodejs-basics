56c081e174cb83f5c84980e94ea440525e93efa8
VyacheslavMusiienko <chuvachok98@gmail.com>
2023-06-16 14:42:41 +0300

    feat: implement function in zip folder

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,5 +1,16 @@
+import { createReadStream, createWriteStream } from 'fs';
+import { dirname, join } from 'path';
+import { pipeline } from 'stream/promises';
+import { fileURLToPath } from 'url';
+import { createGzip } from 'zlib';
+
+const currentFilePath = fileURLToPath(import.meta.url);
+const currentDirPath = dirname(currentFilePath);
+const filePath = join(currentDirPath, 'files', 'fileToCompress.txt');
+const filePathZip = join(currentDirPath, 'files', 'archive.gz');
+
 const compress = async () => {
-    // Write your code here 
+  await pipeline(createReadStream(filePath), createGzip(), createWriteStream(filePathZip))
 };
 
 await compress();
