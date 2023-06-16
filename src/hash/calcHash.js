06269617fc5eec35797bf86cf42bbdc1c445867e
VyacheslavMusiienko <chuvachok98@gmail.com>
2023-06-16 13:06:25 +0300

    fix: hash folder

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,11 +1,11 @@
-import { readFileSync } from 'fs';
 import { createHash } from 'crypto';
-import {dirname,join} from 'path';
+import { readFile } from 'fs/promises';
+import { dirname, join } from 'path';
 import { fileURLToPath } from 'url';
 
 async function calculateHash() {
   const filePath = join(dirname(fileURLToPath(import.meta.url)),'files', 'fileToCalculateHashFor.txt');
-  const fileData = readFileSync(filePath);
+  const fileData = await readFile(filePath);
   const hash = createHash('sha256');
   hash.update(fileData);
   const hexHash = hash.digest('hex');
