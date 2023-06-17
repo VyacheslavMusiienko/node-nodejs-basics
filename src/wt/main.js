0bfe5c850323c45bb41d4483950818c4724f8037
VyacheslavMusiienko <chuvachok98@gmail.com>
2023-06-17 11:13:13 +0300

    refactor: function in wt folder

--- src/wt/main.js
+++ src/wt/main.js
@@ -4,17 +4,26 @@ import { fileURLToPath } from 'url';
 import { Worker } from 'worker_threads';
 
 const workerPath = join(dirname(fileURLToPath(import.meta.url)), 'worker.js');
+
 const performCalculations = async () => {
-  const calcNthFibonacci = (workerData) => new Promise((res) => {
-    const worker = new Worker(workerPath, {workerData});
-    worker.on('message', (data) => res({status: 'resolved', data}));
-    worker.on('error', () => res({status: 'error', data: null}));
-  });
+  let num= 10;
 
-  const calculation = new Array(cpus().length).fill(null).map((_, index) => calcNthFibonacci(index + 10));
-  const data = await Promise.all(calculation);
+  const workers = await Promise.allSettled(cpus().map(() => {
+    return new Promise( (resolve, reject) => {
+      const worker = new Worker(workerPath, {
+        workerData: num++,
+      });
+      worker.on('message', msg => resolve(msg));
+      worker.on('error', msg => reject(msg));
+    });
+  }));
 
-  console.log(data);
+  const results = workers.map(({status, value}) => ({
+    status: status === 'fulfilled' ? 'resolved' : 'error',
+    data: status === 'fulfilled' ? value : null
+  }));
+
+  console.table(results);
 };
 
 await performCalculations();
